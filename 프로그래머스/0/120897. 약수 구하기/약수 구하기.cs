8c7ab947d78626a207718b0b143207840436411e
woghks62 <77401512+woghks62@users.noreply.github.com>
2026-08-31 09:30:54 +0900

    [level 0] Title: 약수 구하기, Time: 0.50 ms, Memory: 46.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class Solution {
+    public int[] solution(int n) {
+        
+        int count = 0;
+        
+        for(int i=1; i<=n; i++){
+            if(n % i == 0){
+                count++;
+            }
+        }
+        
+        int[] answer = new int[count];
+        int num = 0;
+        
+        for(int i=1; i<=n; i++){
+            if(n % i == 0){
+                answer[num++] = i;
+            }
+        }
+        
+        return answer;
+    }
+}
