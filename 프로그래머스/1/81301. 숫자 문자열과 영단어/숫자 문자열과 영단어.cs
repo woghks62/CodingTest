4145f07bf31929d90c7b1ffefd4259f64b1eb6ed
woghks62 <77401512+woghks62@users.noreply.github.com>
2026-09-09 22:07:33 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.81 ms, Memory: 47.8 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cs
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cs
@@ -3,65 +3,20 @@ using System;
 public class Solution {
     public int solution(string s) {
         int answer = 0;
-        string str = "";
         
-        for(int i=0; i<s.Length; i++){
-            if(s[i] >= '0' && s[i] <= '9'){
-                str+=s[i];
-            }
-            if(s[i] == 'z'){
-                str+='0';
-                i+=3;
-            }
-            else if(s[i] == 'o'){
-                str+='1';
-                i+=2;
-            }
-            else if(s[i] == 't'){
-                if(s[i+1] == 'w'){
-                    str+='2';
-                    i+=2;
-                }
-                else if(s[i+1] == 'h'){
-                    str+='3';
-                    i+=4;
-                }
-            }
-            else if(s[i] == 'f'){
-                if(s[i+1] == 'o'){
-                    str+='4';
-                    i+=3;
-                }
-                else if(s[i+1] == 'i'){
-                    str+='5';
-                    i+=3;
-                }
-            }
-            
-            else if(s[i] == 's'){
-                if(s[i+1] == 'i'){
-                    str+='6';
-                    i+=2;
-                }
-                else if(s[i+1] == 'e'){
-                    str+='7';
-                    i+=4;
-                }
-            }
-            
-            else if(s[i] == 'e'){
-                str+='8';
-                i+=4;
-            }
-            
-            else if(s[i] == 'n'){
-                str+='9';
-                i+=3;
-            }
-        }
+        s = s.Replace("zero","0");
+        s = s.Replace("one","1");
+        s = s.Replace("two","2");
+        s = s.Replace("three","3");
+        s = s.Replace("four","4");
+        s = s.Replace("five","5");
+        s = s.Replace("six","6");
+        s = s.Replace("seven","7");
+        s = s.Replace("eight","8");
+        s = s.Replace("nine","9");
         
+        answer = int.Parse(s);
         
-        answer = int.Parse(str);
         return answer;
     }
 }
