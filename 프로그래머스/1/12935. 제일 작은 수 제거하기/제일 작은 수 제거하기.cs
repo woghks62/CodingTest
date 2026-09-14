f52379399385239f35f4527740806ec649750e86
woghks62 <77401512+woghks62@users.noreply.github.com>
2026-09-15 00:34:58 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.70 ms, Memory: 50.7 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.cs
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.cs
@@ -2,24 +2,22 @@ using System;
 
 public class Solution {
     public int[] solution(int[] arr) {
-        int len = arr.Length-1;
+        int min = Int32.MaxValue;
+        int num = 0;
         int[] answer;
-        if(len == 0){
+        answer = new int[arr.Length-1];
+        
+        if(arr.Length-1 == 0){
             answer = new int[1] {-1};
             return answer;
         }
-        
-        answer = new int[len];
-        
-        int min = Int32.MaxValue;
-        
+ 
         for(int i=0; i<arr.Length; i++){
             if(min > arr[i]){
                 min = arr[i];
             }
         }
         
-        int num = 0;
         for(int i=0; i<arr.Length; i++){
             if(arr[i] != min){
                 answer[num++] = arr[i];
